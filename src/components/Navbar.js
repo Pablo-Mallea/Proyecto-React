14a423d8cb9cfd886e1b55e5f31f988cd6e8d4dd
Pablo-Mallea <pablomallea02@gmail.com>
2022-08-17 18:00:20 -0300

    NavBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <h1><a href=" ">Korra</a></h1>
+            <ul>
+                <li><a href=" ">Inicio</a></li>
+                <li><a href=" ">Productos</a></li>
+                <li><a href=" ">Contacto</a></li>
+                <li><a href=" ">Carrito</a></li>
+
+            </ul>
+            <button type="submit" className="btn-log">Login</button>
+        </nav>
+    )
+}
---
+++ src/components/Section.js
@@ -0,0 +1,7 @@
+export const Section = () => {
+    return(
+        <section>
+            <h3>Contenido...</h3>
+        </section>
+    )
+}
