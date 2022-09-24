5388a1276f0495dc28daa74ba4a091eba1794250
Pablo-Mallea <pablomallea02@gmail.com>
2022-09-24 19:52:51 -0300

    Firebase 2

--- src/components/Cart/Cartitem/CartItem.js
+++ src/components/Cart/Cartitem/CartItem.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { CartContext } from "../../Context/CartContext";
 import "./CartItem.scss";
 import { CgTrash } from "react-icons/cg";
@@ -9,7 +9,6 @@ export const CartItem = ({ item }) => {
   const {removeItem } = useContext(CartContext)
   /* console.log(item)
   console.log(cart) */
-  const [cant, setCant] = useState(item.cantidad)
 
   return (
     <div className="cartProduct">
@@ -18,7 +17,7 @@ export const CartItem = ({ item }) => {
       </div>
       <h3>{item.nombre}</h3>
       <p>Precio: ${item.precio}</p>
-      <p>Cantidad: {cant}</p>
+      <p>Cantidad: ${item.cant}</p>
 
       <button onClick={()=> removeItem(item.id)} className="eliminar">
         <CgTrash className="eliminarIcon"/>
